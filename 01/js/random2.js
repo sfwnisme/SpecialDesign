d0395f88c5649ebfbec67bf69fd22f54f2b53859
Safwan <sfwnisme@gmail.com>
2022-12-25 13:47:56 +0300

    .

--- 01/js/random2.js
+++ 01/js/random2.js
@@ -1,26 +1,39 @@
 export default function randomBackground() {
-  let bg = document.querySelector(".landing-page");
-  console.log(bg);
-  let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
-  setInterval(() => {
-    let randomImgs = Math.floor(Math.random() * imgsArray.length);
-    console.log(randomImgs);
-    bg.style.backgroundImage = `url("imgs/${imgsArray[randomImgs]}")`;
-  }, 1000);
+  let backgroundInterval;
+  let backgroundOption = true;
 
+  randomImageFunction();
+  function randomImageFunction() {
+    if (backgroundOption === true) {
+      let bg = document.querySelector(".landing-page");
+      let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+      backgroundInterval = setInterval(() => {
+        let randomImgs = Math.floor(Math.random() * imgsArray.length);
+        console.log(randomImgs);
+        bg.style.backgroundImage = `url("imgs/${imgsArray[randomImgs]}")`;
+      }, 1000);
+    } else {
+      clearInterval(backgroundInterval);
+    }
+  }
   /**
    ********@switchButtons
    */
   let swt = document.querySelectorAll(".random-backgrounds span");
-  let backgroundInterval;
-  let backgroundOption = true;
 
   swt.forEach((span) => {
     span.addEventListener("click", (e) => {
       e.target.parentElement.querySelectorAll(".active").forEach((e) => {
         e.classList.remove("active");
       });
-      console.log(e.parentElement.child);
+      if (e.target.dataset.background === "yes") {
+        backgroundOption = true;
+        randomImageFunction();
+      } else {
+        backgroundOption = false;
+        clearInterval(backgroundInterval);
+      }
       e.target.classList.add("active");
     });
   });
