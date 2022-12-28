01a42f88309ca65a3825f35e86c58c92501c2a9a
Safwan <sfwnisme@gmail.com>
2022-12-28 15:39:19 +0300

    .

--- js/extensions/animation-onscroll.js
+++ js/extensions/animation-onscroll.js
@@ -32,6 +32,38 @@
   [] pageYOffset or scrollY => the height of the window scrollY
 */
 
+/* ==========[Elzero Solutoin]========== */
+// export default function animate() {
+//   console.log("Animation On Scroll");
+
+//   window.addEventListener("scroll", () => {
+//     let skills = document.querySelector(".skills");
+//     let prog = document.querySelectorAll(".skill-progress span");
+//     let skillsOffsetTop = skills.offsetTop;
+//     console.log(skillsOffsetTop);
+//     let skillsOffsetHeight = skills.offsetHeight;
+//     console.log(skillsOffsetHeight);
+//     let windowInnerHeight = window.innerHeight;
+//     console.log(windowInnerHeight);
+//     let windowPageYOffset = window.pageYOffset;
+//     console.log(windowPageYOffset);
+//     // if the height of the window higher than this value start the animation
+//     if (
+//       windowPageYOffset >
+//       skillsOffsetTop + skillsOffsetHeight - windowInnerHeight - 300
+//     ) {
+//       prog.forEach((e) => {
+//         e.style.width = e.dataset.progress;
+//       });
+//     }
+//     console.log(
+//       `${skillsOffsetTop} , ${skillsOffsetHeight} , ${windowInnerHeight}`
+//     );
+//     console.log(skillsOffsetTop + skillsOffsetHeight - windowInnerHeight - 300);
+//   });
+// }
+
+/* ==========[My Solutoin]========== */
 export default function animate() {
   console.log("Animation On Scroll");
 
@@ -39,20 +71,22 @@ export default function animate() {
     let skills = document.querySelector(".skills");
     let prog = document.querySelectorAll(".skill-progress span");
     let skillsOffsetTop = skills.offsetTop;
-    console.log(skillsOffsetTop);
     let skillsOffsetHeight = skills.offsetHeight;
-    console.log(skillsOffsetHeight);
     let windowInnerHeight = window.innerHeight;
-    console.log(windowInnerHeight);
     let windowPageYOffset = window.pageYOffset;
-    console.log(windowPageYOffset);
-    if (
-      windowPageYOffset >
-      skillsOffsetTop + skillsOffsetHeight - windowInnerHeight - 300
-    ) {
-      console.log("=================================");
+
+    // the range of the animation
+    let more = skillsOffsetTop + skillsOffsetHeight - windowInnerHeight * 1.6;
+    let less = skillsOffsetTop + skillsOffsetHeight + windowInnerHeight * -0.5;
+    console.log(`more${more}`);
+    console.log(`less${less}`);
+    if (windowPageYOffset > more && windowPageYOffset < less) {
       prog.forEach((e) => {
         e.style.width = e.dataset.progress;
+      });
+    } else {
+      prog.forEach((e) => {
+        e.style.width = 0;
       });
     }
   });
