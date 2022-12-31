5567d18f286d92e85fe6042c8e9837f15d50fd2c
sfwnisme <sfwnisme@gmail.com>
2022-12-31 07:53:23 +0300

    .

--- js/extensions/open_image.js
+++ js/extensions/open_image.js
@@ -1,25 +1,63 @@
+// export default function openImage() {
+//   const img = document.querySelectorAll(".images-box img");
+
+//   img.forEach((e) => {
+//     e.addEventListener("click", (e) => {
+//       let overlay = document.createElement("div");
+//       document.body.append(overlay);
+//       overlay.className = "popup-overlay";
+
+//       // popup-box
+//       let popBox = document.createElement("div");
+//       popBox.className = "popup-box";
+
+//       let popImg = document.createElement("img");
+//       popImg.src = e.target.src;
+//       popBox.append(popImg);
+//       document.body.append(popBox);
+//       if (popBox.classList.contains("popup-box")) {
+//         popBox.addEventListener("click", (x) => {
+//           x.target.classList.remove("popup-box");
+//         });
+//       }
+//     });
+//   });
+// }
+
 export default function openImage() {
   const img = document.querySelectorAll(".images-box img");
-
-  img.forEach((e) => {
-    e.addEventListener("click", (e) => {
-      let overlay = document.createElement("div");
-      document.body.append(overlay);
+  img.forEach((img) => {
+    img.addEventListener("click", (e) => {
+      const overlay = document.createElement("div");
       overlay.className = "popup-overlay";
 
-      // popup-box
-      let popBox = document.createElement("div");
+      // image box
+      const popBox = document.createElement("div");
       popBox.className = "popup-box";
 
-      let popImg = document.createElement("img");
+      // img
+      const popImg = document.createElement("img");
       popImg.src = e.target.src;
+
+      // Alt
+      const alt = document.createElement("h3");
+      alt.className = "alt";
+      alt.innerText = e.target.alt;
+
+      // closing image action
+      const x = document.createElement("span");
+      x.innerText = "x";
+      x.classList.add("close-button");
+      x.addEventListener("click", () => {
+        overlay.remove();
+      });
+
+      //append
       popBox.append(popImg);
-      document.body.append(popBox);
-      if (popBox.classList.contains("popup-box")) {
-        popBox.addEventListener("click", (x) => {
-          x.target.classList.remove("popup-box");
-        });
-      }
+      popBox.append(alt);
+      overlay.append(x);
+      overlay.append(popBox);
+      document.body.append(overlay);
     });
   });
 }
