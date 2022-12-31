5eece6520406669f9ed8fa3e2e14c98c94a0d3b0
sfwnisme <sfwnisme@gmail.com>
2022-12-31 07:58:24 +0300

    .

--- js/extensions/open_image.js
+++ js/extensions/open_image.js
@@ -1,29 +1,3 @@
-// export default function openImage() {
-//   const img = document.querySelectorAll(".images-box img");
-
-//   img.forEach((e) => {
-//     e.addEventListener("click", (e) => {
-//       let overlay = document.createElement("div");
-//       document.body.append(overlay);
-//       overlay.className = "popup-overlay";
-
-//       // popup-box
-//       let popBox = document.createElement("div");
-//       popBox.className = "popup-box";
-
-//       let popImg = document.createElement("img");
-//       popImg.src = e.target.src;
-//       popBox.append(popImg);
-//       document.body.append(popBox);
-//       if (popBox.classList.contains("popup-box")) {
-//         popBox.addEventListener("click", (x) => {
-//           x.target.classList.remove("popup-box");
-//         });
-//       }
-//     });
-//   });
-// }
-
 export default function openImage() {
   const img = document.querySelectorAll(".images-box img");
   img.forEach((img) => {
